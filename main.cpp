b378b908994c34adb0f69675a2d2b05342350f32
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2018-02-26 12:32:31 -0600

    falta el minimo

--- main.cpp
+++ main.cpp
@@ -8,9 +8,42 @@
 
 using namespace std;
 
+int minimo(int i, int j, int D[100][100], int d[100]){
+  int min;
+  
+}
+
+
+int MultiplicacionM(int d[100], int D[100][100], int n){
+  int j = 0;
+  for(int diag = 1; diag<=n-1; diag++){
+    for(int i = 1; i <= n-diag; i++){
+      j = i + diag;
+      D[i][j] = minimo(i, j, D, d);
+    }
+  }
+  return D[1][n];
+}
+
 int main(){
 
-  /* Code Here */
+  int N; //the length of the stick
+  int Cut; //how many cuts there are gonna be
+
+  cin >> N;
+  cin >> Cut;
+  int Tabla[100];
+  int Adyacentes[100][100];
+  Tabla[0] = 0;
+  for(int i = 1; i <= Cut; i++){// to store where the cuts are gonna be
+    cin >> Tabla[i];
+  }
+  Tabla[Cut+1] = N; //store the lenght of the stick;
+
+  // para llenar la diagonal principal
+  for(int i = 0; i <= Cut+1; i++){
+    Adyacentes[i][i] = 0;
+  }
 
   return 0;
 }
