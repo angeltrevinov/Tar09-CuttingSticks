365065459582157ff276686cae0cf78909b8b289
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2018-02-26 09:12:56 -0600

    adding maing

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+  Angel Odiel Treviño Villanueva
+  A01336559
+  Tarea: Tar09 - Cutting Sticks Fecha:26/feb  Materia: Analisis y Diseño de Algoritmos
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  /* Code Here */
+
+  return 0;
+}
